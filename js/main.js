3576785604f25935ff04736aff2ea205cc1c5d95
dmt100 <80130942+dmt100@users.noreply.github.com>
2021-07-12 01:30:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(window).on('load', function() { // https://youtu.be/vW5_u9UuwGo?t=576
+
+    //Vide.js - video background
+    $('#header').vide('./video/mountains_in_the_fog', {
+        bgColor: '#64947b'
+    });
+    $('#portfolio').vide('./video/programming_white_dark', {
+        bgColor: '#64947b'
+    });
+});
